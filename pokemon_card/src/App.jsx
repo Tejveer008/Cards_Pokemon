a7b650dacc615feff28038c9d17626b620924e2a
Tejveer008 <tejveer.sf@gmail.com>
2024-10-23 20:31:43 +0530

    commit

--- pokemon_card/src/App.jsx
+++ pokemon_card/src/App.jsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import { useEffect } from "react";
-
+import { PokemonCards } from "./PokemonCards";
 export const App = () => {
 
-  const API = "https://pokeapi.co/api/v2/pokemon?limit=21&offset=0";
+  const [pokemon, setPokemon] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const API = "https://pokeapi.co/api/v2/pokemon?limit=24&offset=0";
 
   const fetchPokemon = async () => {
     try {
@@ -10,16 +14,24 @@ export const App = () => {
       const data = await res.json();
       // console.log(data);
 
-      const detailPokemonData = data.results.map(async (curPoke) => {
-        // console.log(curPoke.url);
-
-        // const res = await fetch(curPoke.url);
-        // const data = await res.json();
-        console.log(curPoke.url);
+      const detailedPokemonData = data.results.map(async (curPoke) => {
+        const res = await fetch(curPoke.url);
+        const data = await res.json();
+        console.log(data);
+        return data;
       });
+      // console.log(detailedPokemonData);
+
+      const detailedResponses = await Promise.all(detailedPokemonData);
+      // console.log(detailedResponses);
+
+      setPokemon(detailedResponses);
+      setLoading(false);
 
     } catch (error) {
       console.log(error);
+      setLoading(false);
+      setError(error);
 
     }
   };
@@ -27,9 +39,36 @@ export const App = () => {
   useEffect(() => {
     fetchPokemon();
   }, []);
+
+  if (loading) {
+    return <div>
+      <h1>Loading....</h1>
+    </div>
+  }
+
+
+  if (error) {
+    return <div>
+      <h1>{error.message}</h1>
+    </div>
+  }
+
   return (
     <>
-      <h1>Hello</h1>
+      <section className="container">
+        <header>
+          <h1>Lets catch Pokemon</h1>
+        </header>
+        <div>
+          <ul className="cards">
+            {
+              pokemon.map((curPoke) => {
+                return <PokemonCards key={curPoke.id} pokemonData={curPoke} />
+              })
+            }
+          </ul>
+        </div>
+      </section>
     </>
   )
 };
