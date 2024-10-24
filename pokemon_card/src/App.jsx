270492befc39fd01fc7e1ed16c206f4d96ca9f19
Tejveer008 <tejveer.sf@gmail.com>
2024-10-24 20:05:05 +0530

    commit

--- pokemon_card/src/App.jsx
+++ pokemon_card/src/App.jsx
@@ -1,38 +1,36 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { PokemonCards } from "./PokemonCards";
-export const App = () => {
 
+export const App = () => {
   const [pokemon, setPokemon] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [search, setSearch] = useState("");
+
   const API = "https://pokeapi.co/api/v2/pokemon?limit=24&offset=0";
+  // subscribe to thapa technical youtube channel: https://www.youtube.com/thapatechnical
 
   const fetchPokemon = async () => {
     try {
       const res = await fetch(API);
       const data = await res.json();
-      // console.log(data);
+      //   console.log(data);
 
-      const detailedPokemonData = data.results.map(async (curPoke) => {
-        const res = await fetch(curPoke.url);
+      const detailedPokemonData = data.results.map(async (curPokemon) => {
+        const res = await fetch(curPokemon.url);
         const data = await res.json();
-        console.log(data);
         return data;
       });
-      // console.log(detailedPokemonData);
+      //   console.log(detailedPokemonData);
 
       const detailedResponses = await Promise.all(detailedPokemonData);
-      // console.log(detailedResponses);
-
+      console.log(detailedResponses);
       setPokemon(detailedResponses);
       setLoading(false);
-
     } catch (error) {
       console.log(error);
       setLoading(false);
       setError(error);
-
     }
   };
 
@@ -40,36 +38,55 @@ export const App = () => {
     fetchPokemon();
   }, []);
 
-  if (loading) {
-    return <div>
-      <h1>Loading....</h1>
-    </div>
-  }
+  //search functionality
 
+  const searchData = pokemon.filter((curPokemon) =>
+    curPokemon.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  if (loading) {
+    return (
+      <div>
+        <h1>Loading....</h1>
+      </div>
+    );
+  }
 
   if (error) {
-    return <div>
-      <h1>{error.message}</h1>
-    </div>
+    return (
+      <div>
+        <h1>{error.message}</h1>
+      </div>
+    );
   }
+
+  // subscribe to thapa technical youtube channel: https://www.youtube.com/thapatechnical
 
   return (
     <>
       <section className="container">
         <header>
-          <h1>Lets catch Pokemon</h1>
+          <h1> Lets Catch Pokémon</h1>
         </header>
+        <div className="pokemon-search">
+          <input
+            type="text"
+            placeholder="search Pokemon"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
         <div>
           <ul className="cards">
-            {
-              pokemon.map((curPoke) => {
-                return <PokemonCards key={curPoke.id} pokemonData={curPoke} />
-              })
-            }
+            {/* {pokemon.map((curPokemon) => { */}
+            {searchData.map((curPokemon) => {
+              return (
+                <PokemonCards key={curPokemon.id} pokemonData={curPokemon} />
+              );
+            })}
           </ul>
         </div>
       </section>
     </>
-  )
+  );
 };
-export default App;
